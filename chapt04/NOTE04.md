1481002e6ec3617aabeecf8a063ae28a716b43f5
PhlipTung <philip.dongfei@gmail.com>
2023-11-20 16:14:06 +0800

    chapt04 Head Aches(Solution..Summary)

--- chapt04/NOTE04.md
+++ chapt04/NOTE04.md
@@ -18,6 +18,10 @@ and u64/i64(unsigned/signed 64-bit integer) if you want finer control over how l
 
 ### Reading Bytes Versus Characters
 
+Before continuing, you should understand the difference between reading *bytes* and *characters* from a file. In the early 1960s, the American Standard Code for Information Interchange (ASCII, pronounced *as\-key*) table of 128 characters represented all possible text elements in computing. It takes only seven bits (2^7^ = 128) to represent this many characters. Usually a byte consists of eight bits, so the notion of byte and character were interchangeable.
+
+Since the creation of Unicode (Universal Coded Character Set) to represent all the writing systems of the world (and even emojis),  some characters may require up to four bytes. The Unicode standard defines several ways to encode characters, including UTF\-8 (Unicode Transformation Format using eight bits).  
+
 ## Solution
 
 ### Reading a File Line by Line
@@ -26,8 +30,18 @@ and u64/i64(unsigned/signed 64-bit integer) if you want finer control over how l
 
 ### Reading Bytes from a File
 
+>The **take** method from the **std::io::Read** trait expects its argument to be the type **u64**, but I have a **usize**. I *cast* or convert the value using the **as keyword**.
+
+As you saw in the case of selecting only part of a multibyte character, converting bytes to characters could fail because strings in Rust must be valid UTF-8. The **string::from_utf8 function** will return an **Ok** only if the string is valid, but **string::from_utf8_lossy** will convert invalid UTF-8 sequences to the *unknown* or *replacement* character.
+
 ### Printing the File Separators
 
+
+
 ## Going Further
+
+Consider implementing how the GNU head handles numeric values with suffixes and negative values.
+You could also add an option for selecting characters in addition to bytes.
+
 
 ## Summary
--- chapt04/headr/src/lib.rs
+++ chapt04/headr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -19,20 +21,19 @@ pub fn get_args() -> MyResult<Config> {
         .arg(
             Arg::with_name("lines")
                 .short("n")
-                .long("K_lines")
+                .long("lines")
                 .value_name("LINES")
-                .takes_value(true)
+                .help("print the first K lines instead of the first 10;")
                 .default_value("10")
-                .conflicts_with("bytes")
-                .help("print the first K lines instead of the first 10;"),
 
         )
         .arg(
             Arg::with_name("bytes")
                 .short("c")
-                .long("K_bytes")
+                .long("bytes")
                 .value_name("BYTES")
                 .takes_value(true)
+                .conflicts_with("lines")
                 .help("print the first K bytes of each file."),
 
         )
@@ -46,17 +47,99 @@ pub fn get_args() -> MyResult<Config> {
         )
         .get_matches();
     // TODO: str to int
-    let lines = matches.value_of("lines").parse()::<usize>.unwrap();
-    let bytes = matches.value_of("bytes").parse()::<option<usize>>;
+    let lines = matches.value_of("lines")
+        .map(parse_positive_int)
+        .transpose() // Option::transpose will turn this into a Result<Option>
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+    let bytes = matches.value_of("bytes")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal byte count -- {}", e))?;
 
     Ok(Config {
         files: matches.values_of_lossy("files").unwrap(),
-        lines: 
+        lines: lines.unwrap(), 
         bytes,
     })
 }
 
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val)),//Err(val.into()) or Err(Into::into(val)),
+
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // 3 is an OK integer
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // Any string is an error
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // A zero is an error
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    let num_files = config.files.len();
+
+    for (file_num, filename) in config.files.iter().enumerate() {
+        match open(&filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(mut file) => {
+                if num_files > 1 {
+                    println!(
+                        "{}==> {} <==",
+                        if file_num > 0 { "\n" } else { "" },
+                        &filename
+                    );
+
+                }
+
+                if let Some(num_bytes) = config.bytes {
+                    // read num bytes
+                    let mut buffer = vec![0; num_bytes];
+                    // Use take to read the requested number of bytes
+                    let mut handle = file.take(num_bytes as u64);
+                    let bytes_read = handle.read(&mut buffer)?;
+                    print!("{}",
+                        String::from_utf8_lossy(&buffer[..bytes_read])//Convert the selected bytes into a string, which may not be valid UTF-8.
+                    );
+
+                } else {
+                    // read n lines
+                    let mut line = String::new();
+                    for _ in 0..config.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes == 0 {
+                            break;
+                        }
+                        print!("{}", line);
+                        line.clear();
+                    } 
+
+                } 
+                
+            }
+        }
+    }
     Ok(())
 }
