7594f95d53c7a7a09e8992937e167fb3ca4a8530
PhlipTung <philip.dongfei@gmail.com>
2024-02-21 17:16:20 +0800

    chapt14 Elless Island(Displaying Octal Permissions..[TO DO 'Going Further'])

--- chapt14/lsr/src/lib.rs
+++ chapt14/lsr/src/lib.rs
@@ -1,10 +1,16 @@
+mod owner;
+
+use chrono::{DateTime, Local};
 use clap::{App, Arg};
-use std::path::PathBuf;
+use owner::Owner;
 use std::{
     error::Error,
     fs,
+    os::unix::fs::MetadataExt,
+    path::PathBuf
 };
 use tabular::{Row, Table};
+use users::{get_group_by_gid, get_user_by_uid};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -14,6 +20,8 @@ pub struct Config {
     long: bool,
     show_hidden: bool,
 }
+
+
 
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("lsr")
@@ -92,20 +100,36 @@ fn find_files(
 
 fn format_output(paths: &[PathBuf]) -> MyResult<String> {
     //         1   2     3     4     5     6     7     8
-    let fmt = "{:<}{:<}  {:>}  {:<}  {:<}  {:>}  {:<}  {:<}";
+    let fmt = "{:<}{:<} {:>} {:<} {:<} {:>} {:<} {:<}";
     let mut table = Table::new(fmt);
 
     for path in paths {
+        let metadata = path.metadata()?;
+
+        let uid = metadata.uid();
+        let user = get_user_by_uid(uid)
+            .map(|u| u.name().to_string_lossy().into_owned())
+            .unwrap_or_else(|| uid.to_string());
+
+        let gid = metadata.gid();
+        let group = get_group_by_gid(gid)
+            .map(|g| g.name().to_string_lossy().into_owned())
+            .unwrap_or_else(|| gid.to_string());
+
+        let file_type = if path.is_dir() { "d" } else { "-" };
+        let perms = format_mode(metadata.mode());
+        let modified: DateTime<Local> = DateTime::from(metadata.modified()?);
+
         table.add_row(
             Row::new()
-                .with_cell("") // 1 "d" or "-"
-                .with_cell("") // 2 permissions 
-                .with_cell("") // 3 number of links 
-                .with_cell("") // 4 user name 
-                .with_cell("") // 5 group name 
-                .with_cell("") // 6 size 
-                .with_cell("") // 7 modification 
-                .with_cell("") // 8 path 
+                .with_cell(file_type) // 1 "d" or "-"
+                .with_cell(perms) // 2 permissions 
+                .with_cell(metadata.nlink()) // 3 number of links 
+                .with_cell(user) // 4 user name 
+                .with_cell(group) // 5 group name 
+                .with_cell(metadata.len()) // 6 size 
+                .with_cell(modified.format("%b %d %y %H:%M")) // 7 modification 
+                .with_cell(path.display()), // 8 path 
 
         );
     }
@@ -113,10 +137,89 @@ fn format_output(paths: &[PathBuf]) -> MyResult<String> {
     Ok(format!("{}", table))
 }
 
+fn long_match(
+    line: &str,
+    expected_name: &str,
+    expected_perms: &str,
+    expected_size: Option<&str>,
+) {
+    let parts: Vec<_> = line.split_whitespace().collect();
+    assert!(parts.len() > 0 && parts.len() <= 10);
+
+    let perms = parts.get(0).unwrap();
+    assert_eq!(perms, &expected_perms);
+
+    if let Some(size) = expected_size {
+        let file_size = parts.get(4).unwrap();
+        assert_eq!(file_size, &size);
+    }
+
+    let display_name = parts.last().unwrap();
+    assert_eq!(display_name, &expected_name);
+}
+
+/// Given an octal number like 0o500 and an [`Owner`],
+/// return a string like "r-x"
+pub fn mk_triple(mode: u32, owner: Owner) -> String {
+    let [read, write, execute] = owner.masks();
+    format!(
+        "{}{}{}",
+        if mode & read == 0 { "-" } else { "r" },
+        if mode & write == 0 { "-" } else { "w" },
+        if mode & execute == 0 { "-" } else { "x" },
+    )
+}
+
+
+/// Given a file mode in octal format like 0o751
+/// return a string like "rwxr-x--x"
+// book function
+fn format_mode(mode: u32) -> String {
+    format!(
+        "{}{}{}",
+        mk_triple(mode, Owner::User),
+        mk_triple(mode, Owner::Group),
+        mk_triple(mode, Owner::Other),
+    )
+}
+
+/// Given a file mode in octal format like 0o751
+/// return a string like "rwxr-x--x"
+// my function
+fn my_format_mode(mode: u32) -> String {
+    let changemode = |mode: u32, mask: &[u32], result: &mut String| {
+        for (i, x) in mask.iter().enumerate() {
+            if (x & mode) == mask[i] {
+                match i {
+                    0 => result.push('r'), 
+                    1 => result.push('w'), 
+                    2 => result.push('x'), 
+                    _ => eprintln!("mask error!"),
+                }
+            } else {
+                result.push('-');
+            } 
+        }
+    };
+    let mut result = String::new();
+    let user_mask = vec![0o400, 0o200, 0o100];
+    let group_mask = vec![0o040, 0o020, 0o010];
+    let other_mask = vec![0o004,0o002,0o001];
+    changemode(mode, &user_mask, &mut result);
+    changemode(mode, &group_mask, &mut result);
+    changemode(mode, &other_mask, &mut result);
+    result
+}
+
 pub fn run(config: Config) -> MyResult<()> {
     let paths = find_files(&config.paths, config.show_hidden)?;
-    for path in paths {
-        println!("{}", path.display());
+    if config.long {
+        println!("{}", format_output(&paths)?);
+    } else {
+        for path in paths {
+            println!("{}", path.display());
+        }
+
     }
 
     Ok(())
@@ -124,7 +227,78 @@ pub fn run(config: Config) -> MyResult<()> {
 
 #[cfg(test)]
 mod test {
-    use super::find_files;
+    use super::{mk_triple, Owner, long_match, find_files, format_mode, format_output};
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_mk_triple() {
+        assert_eq!(mk_triple(0o751, Owner::User), "rwx");
+        assert_eq!(mk_triple(0o751, Owner::Group), "r-x");
+        assert_eq!(mk_triple(0o751, Owner::Other), "--x");
+        assert_eq!(mk_triple(0o600, Owner::Other), "---");
+    }
+
+    #[test]
+    fn test_format_output_one() {
+        let bustle_path = "tests/inputs/bustle.txt";
+        let bustle = PathBuf::from(bustle_path);
+
+        let res = format_output(&[bustle]);
+        assert!(res.is_ok());
+
+        let out = res.unwrap();
+        let lines: Vec<&str> = 
+            out.split("\n").filter(|s| !s.is_empty()).collect();
+        assert_eq!(lines.len(), 1);
+
+        let line1 = lines.first().unwrap();
+        long_match(&line1, bustle_path, "-rw-r--r--", Some("193"));
+
+    }
+
+    #[test]
+    fn test_format_output_two() {
+        let res = format_output(&[
+            PathBuf::from("tests/inputs/dir"),
+            PathBuf::from("tests/inputs/empty.txt"),
+        ]);
+        assert!(res.is_ok());
+
+        let out = res.unwrap();
+        let mut lines: Vec<&str> = 
+            out.split("\n").filter(|s| !s.is_empty()).collect();
+        lines.sort();
+        assert_eq!(lines.len(), 2);
+
+        let empty_line = lines.remove(0);
+        long_match(
+            &empty_line,
+            "tests/inputs/empty.txt",
+            "-rw-r--r--",
+            Some("0"),
+        );
+
+        let dir_line = lines.remove(0);
+        long_match(&dir_line, "tests/inputs/dir", "drwxr-xr-x", None);
+    }
+
+    #[test]
+    fn test_octal() {
+        assert_eq!(0b001 + 0b010, 3);
+        assert_eq!((0o321 / 0o1 % 0o10), 0o1);
+        assert_eq!((0o321 / 0o10 % 0o10) , 0o2);
+        assert_eq!((0o321 / 0o100 % 0o10), 0o3);
+        for n in 0..=7 {
+            println!("{} = {:03b}", n, n);
+        }
+
+    }
+
+    #[test]
+    fn test_format_mode() {
+        assert_eq!(format_mode(0o755), "rwxr-xr-x");
+        assert_eq!(format_mode(0o421), "r---w---x");
+    }
 
     #[test]
     fn test_find_files() {
